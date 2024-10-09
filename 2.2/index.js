3d708f43f44d5b85a27adf5cd4a536bcbf6b7c44
DryPepper <138318004+DryPepper@users.noreply.github.com>
2024-10-09 19:46:05 +0500

    Add files via upload

---
+++ 2.2/index.js
@@ -0,0 +1,17 @@
+let product1 = prompt("Введите название товара")
+let price1 = prompt("Введите цену товара")
+let count1 = prompt("Введите количество товара")
+
+console.log(product1, price1 * count1)
+
+let product2 = prompt("Введите название товара")
+let price2 = prompt("Введите цену товара")
+let count2 = prompt("Введите количество товара")
+
+console.log(product2, price2 * count2)
+
+let product3 = prompt("Введите название товара")
+let price3 = prompt("Введите цену товара")
+let count3 = prompt("Введите количество товара")
+
+console.log(product3, price3 * count3)
